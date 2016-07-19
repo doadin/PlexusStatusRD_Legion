e952615f1b06ad288b09da50ed64cb0ac82560be
stassart <stassart@d2612e12-55b7-4df4-9dce-84da765cb2fd>
2016-07-19 10:58:40 +0000

    Initial version.

---
+++ BrokenIsles.lua
@@ -0,0 +1,43 @@
+-- local zone = "Broken Isles"
+local zoneid = 1007
+
+-- for i=1,1500,1 do
+--    zone = GetMapNameByID(i)
+--    if zone == "Broken Isles" then
+--       print(i)
+--    end
+-- end
+
+-- Ana-Mouz
+GridStatusRaidDebuff:BossNameId(zoneid, 10, "Ana-Mouz")
+
+-- Calamir
+GridStatusRaidDebuff:BossNameId(zoneid, 20, "Calamir")
+
+-- Drugon the Frostblood
+GridStatusRaidDebuff:BossNameId(zoneid, 30, "Drugon the Frostblood")
+
+-- Flotsam
+GridStatusRaidDebuff:BossNameId(zoneid, 40, "Flotsam")
+
+-- Humongris
+GridStatusRaidDebuff:BossNameId(zoneid, 50, "Humongris")
+
+-- Levantus
+GridStatusRaidDebuff:BossNameId(zoneid, 60, "Levantus")
+
+-- Na'zak the Fiend
+GridStatusRaidDebuff:BossNameId(zoneid, 70, "Na'zak the Fiend")
+
+-- Nithogg
+GridStatusRaidDebuff:BossNameId(zoneid, 80, "Nithogg")
+
+-- Shar'thos
+GridStatusRaidDebuff:BossNameId(zoneid, 90, "Shar'thos")
+
+-- The Soultakers
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "The Soultakers")
+
+-- Withered J'im
+GridStatusRaidDebuff:BossNameId(zoneid, 110, "Withered J'im")
+
