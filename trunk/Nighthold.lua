1be5894fc60cbb4d2fc1e96b209138c6129d0eee
tptbk <tptbk@d2612e12-55b7-4df4-9dce-84da765cb2fd>
2016-07-28 01:41:13 +0000

    A More Complete Boss Debuffs for Nighthold

--- trunk/Nighthold.lua
+++ trunk/Nighthold.lua
@@ -39,16 +39,34 @@ GridStatusRaidDebuff:DebuffId(zoneid, 216040, 56, 5, 5, true, false) -- Burning 
 
 -- Krosus
 GridStatusRaidDebuff:BossNameId(zoneid, 60, "Krosus")
+-- Searing Brand (non-dispellable, Stacks)(TODO)
+GridStatusRaidDebuff:DebuffId(zoneid, 208203, 62, 5, 5) -- Isolated Rage (non-dispellable)
 
 -- High Botanist Tel'arn
 GridStatusRaidDebuff:BossNameId(zoneid, 70, "High Botanist Tel'arn")
+GridStatusRaidDebuff:DebuffId(zoneid, 218502, 71, 5, 5, false, true) -- Recursive Strikes (non-dispellable, stacks)
+GridStatusRaidDebuff:DebuffId(zoneid, 219049, 72, 5, 5) -- Toxic Spores (non-dispellable)
+GridStatusRaidDebuff:DebuffId(zoneid, 218424, 73, 5, 5, false, true) -- Parasitic Fetter (dispellable)
 
 -- Star Augur Etraeus
 GridStatusRaidDebuff:BossNameId(zoneid, 80, "Star Augur Etraeus")
+GridStatusRaidDebuff:DebuffId(zoneid, 206585, 81, 5, 5, false, true) -- Absolute Zero (non-dispellable, stacks)
+GridStatusRaidDebuff:DebuffId(zoneid, 206936, 82, 5, 5) -- Icy Ejection (non-dispellable, stacks)
+GridStatusRaidDebuff:DebuffId(zoneid, 206388, 83, 5, 5, false, true) -- Felburst (non-dispellable, stacks)
+GridStatusRaidDebuff:DebuffId(zoneid, 205649, 84, 5, 5, false, true) -- Fel Ejection (non-dispellable, stacks)
+GridStatusRaidDebuff:DebuffId(zoneid, 206965, 85, 5, 5) -- Voidburst (non-dispellable)
+GridStatusRaidDebuff:DebuffId(zoneid, 207143, 85, 5, 5) -- Void Ejection (non-dispellable)
 
 -- Grand Magistrix Elisande
 GridStatusRaidDebuff:BossNameId(zoneid, 90, "Grand Magistrix Elisande")
 
 -- Gul'dan
 GridStatusRaidDebuff:BossNameId(zoneid, 100, "Gul'dan")
+GridStatusRaidDebuff:DebuffId(zoneid, 212568, 101, 5, 5, true, false) -- Drain (dispellable)
+GridStatusRaidDebuff:DebuffId(zoneid, 206883, 102, 5, 5, false, true) -- Soul Vortex (non-dispellable, stacks)
+GridStatusRaidDebuff:DebuffId(zoneid, 206222, 103, 5, 5) -- Bonds of Fel (non-dispellable)
+GridStatusRaidDebuff:DebuffId(zoneid, 206221, 104, 5, 5) -- Empowered Bonds of Fel (non-dispellable)
+GridStatusRaidDebuff:DebuffId(zoneid, 208672, 105, 5, 5) -- Carrion Wave (non-dispellable)
+GridStatusRaidDebuff:DebuffId(zoneid, 208903, 106, 5, 5) -- Burning Claws (non-dispellable)
+GridStatusRaidDebuff:DebuffId(zoneid, 208802, 107, 5, 5) -- Soul Corrosion (non-dispellable)
 
