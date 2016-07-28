f96741a971c074485d2b3778ca9a0aba6065dc41
tptbk <tptbk@d2612e12-55b7-4df4-9dce-84da765cb2fd>
2016-07-28 02:24:33 +0000

    Add More Comments for Nighthold

--- Nighthold.lua
+++ Nighthold.lua
@@ -3,10 +3,14 @@ local zoneid = 1088
 
 -- Skorpyron
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Skorpyron")
+-- Crystalline Scorpid
 GridStatusRaidDebuff:DebuffId(zoneid, 204766, 11, 5, 5, false, true) -- Energy Surge (non-dispellable, stacks)
+-- Chromatic Exoskeleton
 GridStatusRaidDebuff:DebuffId(zoneid, 214657, 12, 5, 5) -- Acidic Fragments (non-dispellable)
 GridStatusRaidDebuff:DebuffId(zoneid, 214662, 13, 5, 5) -- Volatile Fragments (non-dispellable)
+-- Arcanoslash
 GridStatusRaidDebuff:DebuffId(zoneid, 211659, 14, 5, 5, false, true) -- Arcane Tether (non-dispellable, stacks)
+
 GridStatusRaidDebuff:DebuffId(zoneid, 204471, 15, 5, 5) -- Focused Blast (non-dispellable)
 
 -- Chronomatic Anomaly
@@ -19,22 +23,28 @@ GridStatusRaidDebuff:DebuffId(zoneid, 212099, 24, 5, 5) -- Temporal Charge (non-
 -- Trilliax
 GridStatusRaidDebuff:BossNameId(zoneid, 30, "Trilliax")
 GridStatusRaidDebuff:DebuffId(zoneid, 206482, 31, 5, 5) -- Arcane Seepage (non-dispellable)
+-- The Cleaner
 GridStatusRaidDebuff:DebuffId(zoneid, 206788, 32, 5, 5, false, true) -- Toxic Slice (non-dispellable)
 
 -- Spellblade Aluriel
 GridStatusRaidDebuff:BossNameId(zoneid, 40, "Spellblade Aluriel")
+-- Chamion of Blades
 GridStatusRaidDebuff:DebuffId(zoneid, 212492, 41, 5, 5) -- Annihilate (non-dispellable)
+-- Master of Frost
 GridStatusRaidDebuff:DebuffId(zoneid, 212587, 42, 5, 5) -- Mark of Frost (non-dispellable)
 -- Frostbitten (non-dispellable, Stacks)(TODO)
 GridStatusRaidDebuff:DebuffId(zoneid, 213083, 44, 5, 5) -- Frozen Tempest (non-dispellable)
 
 -- Tichondrius
 GridStatusRaidDebuff:BossNameId(zoneid, 50, "Tichondrius")
+-- Stage One
 GridStatusRaidDebuff:DebuffId(zoneid, 206480, 51, 5, 5) -- Carrion Plague (non-dispellable)
 GridStatusRaidDebuff:DebuffId(zoneid, 213238, 52, 5, 5) -- Seeker Swarm (non-dispellable)
 GridStatusRaidDebuff:DebuffId(zoneid, 212795, 53, 5, 5) -- Brand of Argus (non-dispellable)
 GridStatusRaidDebuff:DebuffId(zoneid, 208230, 54, 5, 5) -- Feast of Blood (non-dispellable)
+-- The Nightborne Felsworn Spellguard
 GridStatusRaidDebuff:DebuffId(zoneid, 216024, 55, 5, 5) -- Volatile Wound (non-dispellable)
+-- The Legion Sightless Watcher
 GridStatusRaidDebuff:DebuffId(zoneid, 216040, 56, 5, 5, true, false) -- Burning Soul (dispellable)
 
 -- Krosus
@@ -44,16 +54,21 @@ GridStatusRaidDebuff:DebuffId(zoneid, 208203, 62, 5, 5) -- Isolated Rage (non-di
 
 -- High Botanist Tel'arn
 GridStatusRaidDebuff:BossNameId(zoneid, 70, "High Botanist Tel'arn")
+--Arcanist Tel'arn
 GridStatusRaidDebuff:DebuffId(zoneid, 218502, 71, 5, 5, false, true) -- Recursive Strikes (non-dispellable, stacks)
+-- Naturalist Tel'arn
 GridStatusRaidDebuff:DebuffId(zoneid, 219049, 72, 5, 5) -- Toxic Spores (non-dispellable)
 GridStatusRaidDebuff:DebuffId(zoneid, 218424, 73, 5, 5, false, true) -- Parasitic Fetter (dispellable)
 
 -- Star Augur Etraeus
 GridStatusRaidDebuff:BossNameId(zoneid, 80, "Star Augur Etraeus")
+-- Stage Two
 GridStatusRaidDebuff:DebuffId(zoneid, 206585, 81, 5, 5, false, true) -- Absolute Zero (non-dispellable, stacks)
 GridStatusRaidDebuff:DebuffId(zoneid, 206936, 82, 5, 5) -- Icy Ejection (non-dispellable, stacks)
+-- Stage Three
 GridStatusRaidDebuff:DebuffId(zoneid, 206388, 83, 5, 5, false, true) -- Felburst (non-dispellable, stacks)
 GridStatusRaidDebuff:DebuffId(zoneid, 205649, 84, 5, 5, false, true) -- Fel Ejection (non-dispellable, stacks)
+-- Stage Four
 GridStatusRaidDebuff:DebuffId(zoneid, 206965, 85, 5, 5) -- Voidburst (non-dispellable)
 GridStatusRaidDebuff:DebuffId(zoneid, 207143, 86, 5, 5) -- Void Ejection (non-dispellable)
 
@@ -62,11 +77,17 @@ GridStatusRaidDebuff:BossNameId(zoneid, 90, "Grand Magistrix Elisande")
 
 -- Gul'dan
 GridStatusRaidDebuff:BossNameId(zoneid, 100, "Gul'dan")
+-- Stage One
+-- Inquisitor Vethriz
 GridStatusRaidDebuff:DebuffId(zoneid, 212568, 101, 5, 5, true, false) -- Drain (dispellable)
+-- D'zorykx the Trapper
 GridStatusRaidDebuff:DebuffId(zoneid, 206883, 102, 5, 5, false, true) -- Soul Vortex (non-dispellable, stacks)
+-- Gul'dan
 GridStatusRaidDebuff:DebuffId(zoneid, 206222, 103, 5, 5) -- Bonds of Fel (non-dispellable)
 GridStatusRaidDebuff:DebuffId(zoneid, 206221, 104, 5, 5) -- Empowered Bonds of Fel (non-dispellable)
+-- Dreadlords of the Twisting Nether
 GridStatusRaidDebuff:DebuffId(zoneid, 208672, 105, 5, 5) -- Carrion Wave (non-dispellable)
+-- Gul'dan
 GridStatusRaidDebuff:DebuffId(zoneid, 208903, 106, 5, 5) -- Burning Claws (non-dispellable)
 GridStatusRaidDebuff:DebuffId(zoneid, 208802, 107, 5, 5) -- Soul Corrosion (non-dispellable)
 
