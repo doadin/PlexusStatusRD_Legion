db15cbc06786cebe429ca9c84e88d8e6d1571b1b
tptbk <tptbk@d2612e12-55b7-4df4-9dce-84da765cb2fd>
2016-11-09 00:15:08 +0000

    Tagging as r14

---
+++ tags/r14/TrialofValor.lua
@@ -0,0 +1,22 @@
+--local zone = "Trial of Valor"
+local zoneid = 1114
+
+-- Guarm
+GridStatusRaidDebuff:BossNameId(zoneid, 10, "Guarm")
+GridStatusRaidDebuff:DebuffId(zoneid, 228226, 11, 5, 5) -- Flame Lick
+GridStatusRaidDebuff:DebuffId(zoneid, 228246, 12, 5, 5) -- Frost Lick
+GridStatusRaidDebuff:DebuffId(zoneid, 228250, 13, 5, 5) -- Shadow Lick
+
+-- Helya
+GridStatusRaidDebuff:BossNameId(zoneid, 20, "Helya")
+GridStatusRaidDebuff:DebuffId(zoneid, 227903, 21, 5, 5) -- Orb of Corruption
+GridStatusRaidDebuff:DebuffId(zoneid, 228058, 22, 5, 5) -- Orb of Corrosion
+GridStatusRaidDebuff:DebuffId(zoneid, 228054, 23, 5, 5) -- Taint of the Sea
+GridStatusRaidDebuff:DebuffId(zoneid, 193367, 24, 5, 5) -- Fetid Rot
+GridStatusRaidDebuff:DebuffId(zoneid, 227982, 25, 5, 5) -- Bilewater Redox
+
+
+-- Odyn
+GridStatusRaidDebuff:BossNameId(zoneid, 30, "Odyn")
+GridStatusRaidDebuff:DebuffId(zoneid, 227959, 31, 5, 5) -- Storm of Justice
+GridStatusRaidDebuff:DebuffId(zoneid, 227475, 32, 5, 5) -- Cleansing Flame
