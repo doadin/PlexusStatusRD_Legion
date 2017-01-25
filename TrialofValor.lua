41b228a28bb48c05d3f7797035edbc77a83c1224
tptbk <tptbk@d2612e12-55b7-4df4-9dce-84da765cb2fd>
2017-01-25 01:49:49 +0000

    More Trial of Valor

--- TrialofValor.lua
+++ TrialofValor.lua
@@ -13,6 +13,9 @@ GridStatusRaidDebuff:DebuffId(zoneid, 228250, 13, 6, 6) -- Shadow Lick
 GridStatusRaidDebuff:DebuffId(zoneid, 227539, 14, 5, 5) -- Fiery Phlegm
 GridStatusRaidDebuff:DebuffId(zoneid, 227566, 15, 5, 5) -- Salty Spittle
 GridStatusRaidDebuff:DebuffId(zoneid, 227570, 16, 5, 5) -- Dark Discharge
+GridStatusRaidDebuff:DebuffId(zoneid, 228744, 17, 5, 5) -- Flaming Volatile Foam
+GridStatusRaidDebuff:DebuffId(zoneid, 228810, 18, 5, 5) -- Briney Volatile Foam
+GridStatusRaidDebuff:DebuffId(zoneid, 228818, 19, 5, 5) -- Shadowy Volatile Foam
 
 -- Helya
 GridStatusRaidDebuff:BossNameId(zoneid, 20, "Helya")
