6e11e3fa83e972327503f4cde67f0a7a1f5ef641
tptbk <tptbk@d2612e12-55b7-4df4-9dce-84da765cb2fd>
2017-11-28 23:13:52 +0000

    Add/Update Antorus Support

---
+++ trunk/AntorustheBurningThrone.lua
@@ -0,0 +1,95 @@
+-- local zone = "Antorus, the Burning Throne"
+local zoneid = 1188
+
+-- Trash
+
+
+-- Varimathras
+GridStatusRaidDebuff:BossNameId(zoneid, 10, "Varimathras")
+GridStatusRaidDebuff:DebuffId(zoneid, 244042, 11, 6, 6, false, true) -- Marked Prey
+GridStatusRaidDebuff:DebuffId(zoneid, 243999, 12, 6, 6, false, true) -- Dark Fissure
+GridStatusRaidDebuff:DebuffId(zoneid, 244093, 13, 6, 6, false, true) -- Necrotic Embrace
+GridStatusRaidDebuff:DebuffId(zoneid, 243961, 14, 6, 6) -- Misery 
+
+-- Aggramar
+GridStatusRaidDebuff:BossNameId(zoneid, 20, "Aggramar")
+GridStatusRaidDebuff:DebuffId(zoneid, 243431, 21, 6, 6, true, true) -- Taeshalach's Reach
+GridStatusRaidDebuff:DebuffId(zoneid, 245995, 22, 6, 6, false, true) -- Scorching Blaze
+GridStatusRaidDebuff:DebuffId(zoneid, 246014, 23, 6, 6, false, true) -- Searing Tempest
+GridStatusRaidDebuff:DebuffId(zoneid, 244912, 24, 6, 6, false, true) -- Blazing Eruption
+
+-- Portal Keeper Hasabel
+GridStatusRaidDebuff:BossNameId(zoneid, 30, "Portal Keeper Hasabel")
+GridStatusRaidDebuff:DebuffId(zoneid, 245157, 31, 6, 6) -- Everburning Light
+GridStatusRaidDebuff:DebuffId(zoneid, 244853, 32, 6, 6) -- Caustic Detonation
+GridStatusRaidDebuff:DebuffId(zoneid, 245075, 33, 6, 6) -- Hungering Gloom
+GridStatusRaidDebuff:DebuffId(zoneid, 244016, 34, 6, 6, true, true) -- Reality Tear
+GridStatusRaidDebuff:DebuffId(zoneid, 245240, 34, 6, 6) -- Oppressive Gloom
+
+-- Noura, Mother of Flames <Coven of Shivarra>
+GridStatusRaidDebuff:BossNameId(zoneid, 40, "Noura")
+GridStatusRaidDebuff:DebuffId(zoneid, 250095, 41, 6, 6, true, true) -- Machinations of Aman'Thul
+GridStatusRaidDebuff:DebuffId(zoneid, 245586, 42, 6, 6, false, true) -- Chilled Blood
+GridStatusRaidDebuff:DebuffId(zoneid, 245518, 43, 6, 6, true, true) -- Flashfreeze
+GridStatusRaidDebuff:DebuffId(zoneid, 244899, 44, 6, 6, true, true) -- Fiery Strike
+
+-- F'harg
+GridStatusRaidDebuff:BossNameId(zoneid, 50, "F'harg")
+GridStatusRaidDebuff:DebuffId(zoneid, 248815, 51, 6, 6, false, true) -- Enflamed
+GridStatusRaidDebuff:DebuffId(zoneid, 244057, 52, 6, 6) -- Enflame Corruption
+GridStatusRaidDebuff:DebuffId(zoneid, 244768, 53, 6, 6) -- Desolate Gaze
+GridStatusRaidDebuff:DebuffId(zoneid, 254760, 54, 6, 6) -- Corrupting Maw
+GridStatusRaidDebuff:DebuffId(zoneid, 244071, 55, 6, 6, false, true) -- Weight of Darkness
+GridStatusRaidDebuff:DebuffId(zoneid, 248819, 56, 6, 6) -- Siphoned
+GridStatusRaidDebuff:DebuffId(zoneid, 244056, 57, 6, 6) -- Siphon Corruption
+GridStatusRaidDebuff:DebuffId(zoneid, 248815, 58, 6, 6) -- Enflamed
+GridStatusRaidDebuff:DebuffId(zoneid, 251445, 59, 6, 6) -- Smouldering
+GridStatusRaidDebuff:DebuffId(zoneid, 244091, 60, 6, 6, true, true) -- Singed
+GridStatusRaidDebuff:DebuffId(zoneid, 244517, 61, 6, 6, true, true) -- Lingering Flames
+GridStatusRaidDebuff:DebuffId(zoneid, 245098, 62, 6, 6, true, true) -- Decay 
+
+-- Garothi Worldbreaker
+GridStatusRaidDebuff:BossNameId(zoneid, 70, "Garothi Worldbreaker")
+GridStatusRaidDebuff:DebuffId(zoneid, 244395, 71, 6, 6) -- Searing Barrage
+GridStatusRaidDebuff:DebuffId(zoneid, 244410, 72, 6, 6) -- Decimation
+GridStatusRaidDebuff:DebuffId(zoneid, 246848, 73, 6, 6) -- Luring Destruction
+GridStatusRaidDebuff:DebuffId(zoneid, 244536, 74, 6, 6) -- Fel Bombardment
+
+-- Admiral Svirax
+GridStatusRaidDebuff:BossNameId(zoneid, 80, "Admiral Svirax")
+GridStatusRaidDebuff:DebuffId(zoneid, 244910, 81, 6, 6) -- Felshield
+GridStatusRaidDebuff:DebuffId(zoneid, 244172, 82, 6, 6) -- Psychic Assault
+GridStatusRaidDebuff:DebuffId(zoneid, 257974, 83, 6, 6, true, true) -- Chaos Pulse
+GridStatusRaidDebuff:DebuffId(zoneid, 244892, 83, 6, 6, false, true) -- Exploit Weakness
+
+-- Kin'garoth <Engineer of the Apocalypse>
+GridStatusRaidDebuff:BossNameId(zoneid, 90, "Kin'garoth")
+GridStatusRaidDebuff:DebuffId(zoneid, 248061, 91, 6, 6) -- Purging Protocol
+GridStatusRaidDebuff:DebuffId(zoneid, 246706, 92, 6, 6) -- Demolish
+GridStatusRaidDebuff:DebuffId(zoneid, 244312, 93, 6, 6) -- Forging Strike
+
+-- Imonar the Soulhunter
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Imonar the Soulhunter")
+GridStatusRaidDebuff:DebuffId(zoneid, 248321, 101, 6, 6, false, true) -- Conflagration
+GridStatusRaidDebuff:DebuffId(zoneid, 248424, 102, 6, 6) -- Gathering Power
+GridStatusRaidDebuff:DebuffId(zoneid, 247641, 103, 6, 6) -- Stasis Trap
+GridStatusRaidDebuff:DebuffId(zoneid, 247552, 104, 6, 6) -- Sleep Canister
+GridStatusRaidDebuff:DebuffId(zoneid, 247565, 105, 6, 6) -- Slumber Gas
+GridStatusRaidDebuff:DebuffId(zoneid, 247367, 106, 6, 6, true, true) -- Shock Lance
+GridStatusRaidDebuff:DebuffId(zoneid, 247687, 107, 6, 6, true, true) -- Sever
+GridStatusRaidDebuff:DebuffId(zoneid, 250255, 108, 6, 6, true, true) -- Empowered Shock Lance
+
+-- Essence of Eonar
+GridStatusRaidDebuff:BossNameId(zoneid, 110, "Essence of Eonar")
+GridStatusRaidDebuff:DebuffId(zoneid, 248326, 111, 6, 6) -- Rain of Fel
+GridStatusRaidDebuff:DebuffId(zoneid, 248333, 112, 6, 6) -- Meteor Storm
+GridStatusRaidDebuff:DebuffId(zoneid, 248861, 113, 6, 6) -- Spear of Doom
+
+-- Argus the Unmaker
+GridStatusRaidDebuff:BossNameId(zoneid, 120, "Argus the Unmaker")
+GridStatusRaidDebuff:DebuffId(zoneid, 248499, 121, 6, 6, true, true) -- Sweeping Scythe
+GridStatusRaidDebuff:DebuffId(zoneid, 255199, 122, 6, 6, false, true) -- Avatar of Aggramar
+GridStatusRaidDebuff:DebuffId(zoneid, 251570, 123, 6, 6, false, true) -- Soulbomb
+GridStatusRaidDebuff:DebuffId(zoneid, 250669, 124, 6, 6, false, true) -- Soulburst
+GridStatusRaidDebuff:DebuffId(zoneid, 257313, 125, 6, 6, false, true) -- Gift of the Sky
+GridStatusRaidDebuff:DebuffId(zoneid, 257306, 126, 6, 6, false, true) -- Gift of the Sea
